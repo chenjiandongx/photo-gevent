d5838f1803fe1b1d9d4d85062ef98beaee609d98
chenjiandongx <chenjiandongx@qq.com>
2018-06-11 17:13:18 +0800

    Add: 新增图片数量以及断点下载去重操作

--- core.py
+++ core.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 
 import os
-import uuid
+import hashlib
 import logging
 from queue import Queue
 
@@ -14,24 +14,30 @@ from gevent.pool import Pool
 # function in this module).
 monkey.patch_all()
 
-HEADERS = {
-    "X-Requested-With": "XMLHttpRequest",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 "
-    "(KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
-    "Referer": "http://www.mzitu.com",
-}
 
 POOL_MAXSIZE = 512
 WORKERS_MAXSIZE = 16
 DELAY_TIME = 0.25
 REQUEST_TIMEOUT = 8
-MAX_RETRIES = 3
+MAX_RETRIES = 5
 
-URLS_DATA = "data1.txt"
+URLS_DATA = "data.txt"
 PICS_DIR = "pics"
 PICS_EXT = ".jpg"
-PICS_FILENAME_LENGTH = 15
+PICS_FILENAME_LENGTH = 16
 LOG_LEVEL = logging.WARNING
+
+USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 "
+    "(KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
+)
+
+DOMAIN = {"mzitu": "http://i.meizitu.net/", "mmjpg": "http://img.mmjpg.com/"}
+
+HEADERS = {
+    DOMAIN["mzitu"]: {"User-Agent": USER_AGENT, "Referer": "http://www.mzitu.com"},
+    DOMAIN["mmjpg"]: {"User-Agent": USER_AGENT, "Referer": "http://www.mmjpg.com"},
+}
 
 
 class Logger:
@@ -60,12 +66,19 @@ class Downloader:
     def init_queue(self):
         with open(URLS_DATA, "r", encoding="utf8") as f:
             for u in f.readlines():
-                self.urls_queue.put({"http://i.meizitu.net/" + u.strip(): MAX_RETRIES})
+                self.urls_queue.put({u.strip(): MAX_RETRIES})
 
     @staticmethod
     def create_dir():
         if not os.path.exists(PICS_DIR):
             os.mkdir(PICS_DIR)
+
+    @staticmethod
+    def headers(url):
+        if url.startswith(DOMAIN["mzitu"]):
+            return HEADERS.get(DOMAIN["mzitu"])
+        if url.startswith(DOMAIN["mmjpg"]):
+            return HEADERS.get(DOMAIN["mmjpg"])
 
     def download(self):
         while True:
@@ -79,10 +92,19 @@ class Downloader:
                     self.urls_queue.task_done()
                     break
                 try:
-                    resp = requests.get(url, headers=HEADERS, timeout=REQUEST_TIMEOUT).content
-                    file_name = str(uuid.uuid4().hex)[:PICS_FILENAME_LENGTH] + PICS_EXT
-                    with open(os.path.join(PICS_DIR, file_name), "wb") as f:
-                        f.write(resp)
+                    file_name = hashlib.sha224(url.encode("utf8")).hexdigest()[
+                        :PICS_FILENAME_LENGTH
+                    ] + PICS_EXT
+                    file_path = os.path.join(PICS_DIR, file_name)
+                    if os.path.exists(file_path):
+                        self.urls_queue.task_done()
+                        self.logger.warning("Ignore {} has existed".format(file_path))
+                        break
+                    resp = requests.get(
+                        url, headers=self.headers(url), timeout=REQUEST_TIMEOUT
+                    )
+                    with open(file_path, "wb") as f:
+                        f.write(resp.content)
                         self.logger.info("Filename " + file_name)
                     self.urls_queue.task_done()
                 except Exception as e:
@@ -90,7 +112,9 @@ class Downloader:
                     if num >= 1:
                         self.urls_queue.task_done()
                         self.urls_queue.put({url: num - 1})
-                        self.logger.warning("Url {} retry times {}".format(url, MAX_RETRIES - (num - 1)))
+                        self.logger.warning(
+                            "Url {} retry times {}".format(url, MAX_RETRIES - (num - 1))
+                        )
 
     def execute_jobs(self, target):
         for i in range(WORKERS_MAXSIZE):
